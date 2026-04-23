befa0433d4f5c0ee59b51368326b3fbf38017f90
aljafarijehad <aljafarijehad@gmail.com>
2026-04-24 02:34:10 +0300

    Add Menue Screen

--- BankProject_Improved version-12-4-2026/BankProject_Improved version-12-4-2026.cpp
+++ BankProject_Improved version-12-4-2026/BankProject_Improved version-12-4-2026.cpp
@@ -1,7 +1,30 @@
-
 #include <iostream>
+#include "clsBankClient.h"
+#include "clsMainScreen.h"
+//#include <iomanip>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+
+
+	////clsBankClient::ShowClientList();
+
+
+	// clsBankClient::DeleteClient();
+
+	 //clsBankClient::ShowTotalBalance();
+
+	//clsBankClient::UpdateClient();
+	
+	//clsBankClient New = clsBankClient::AddNewClient();
+
+	clsMainScreen:: ShowMainMenuScreen();
+
+
+
+
+	// Client1.Print();
+
+	return 0;
 }
